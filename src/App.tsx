3949765bb62d6e1a95dca0c3f7781a3a8ffe0964
Petter Knutsson <petterknutsson5@gmail.com>
2020-09-18 21:01:14 +0200

    feat: Fetch weather data from API, display temperature #26 #27 #28 #29 #30

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { IUserGeoLocation } from './models';
+import { IWeather } from './models/weather';
+import { kelvinToCelsius } from './helpers';
 
 function App() {
-  const appClass = "App"
+  const GEOLOC_URL = "https://geolocation-db.com/json/";
+  const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const [weather, setWeather] = useState<IWeather>(); // this is the weather data
+  const [userGeolocation, setUserGeolocation] = useState<IUserGeoLocation | undefined>(undefined);
+
+  const getWeatherApiUrl = useCallback(
+    (latitude: number, longitude: number): string => {
+      return `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+    },
+    [API_KEY],
+  )
+
+  /**
+   * get user geolocation including coordinates
+   */
+  useEffect(() => {
+    const fetchGeolocation = (): void => {
+      axios(GEOLOC_URL)
+        .then(res => setUserGeolocation(res.data))
+        .catch(error => {
+          throw error;
+        });
+    };
+    fetchGeolocation();
+  }, []);
+
+  /**
+   * get weather data using user coordinates
+   */
+  useEffect(() => {
+    const lat = userGeolocation?.latitude;
+    const lon = userGeolocation?.longitude;
+    if (!lat || !lon) {
+      return;
+    }
+    
+    const WEATHER_API_URL = getWeatherApiUrl(lat, lon);
+    axios(WEATHER_API_URL)
+      .then(res => setWeather(res.data))
+      .catch(error => {
+        throw error;
+      });
+
+  }, [getWeatherApiUrl, userGeolocation])
+
   return (
-    <div className={appClass}>
+    <div className="App">
       <header className="App-header">
-        <h1>Instagroom</h1>
+        <h1>Weather</h1>
+        {weather && kelvinToCelsius(weather.current.temp)}
       </header>
     </div>
   );
---
+++ src/models/weather.ts
@@ -0,0 +1,18 @@
+export interface IWeather {
+  current: {
+    clouds: number;
+    dew_point: number;
+    dt: number;
+    feels_like: number
+    humidity: number;
+    pressure: number;
+    sunrise: number;
+    sunset: number;
+    temp: number; // this is in kelvin
+    uvi: number;
+    visibility: number;
+    weather: any; // expand later
+    wind_deg: number;
+    wind_speed: number
+  }
+}
