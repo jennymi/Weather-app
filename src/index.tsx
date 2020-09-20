dcf75bab3e177f562f81ff5c9f11379c09349b36
Petter Knutsson <petterknutsson5@gmail.com>
2020-09-21 00:26:22 +0200

    refactor: Create custom hooks for each API call #55

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './misc/serviceWorker';
+import './index.css';
 
 /**
  * Find icons: https://fontawesome.com/icons?d=gallery
@@ -14,7 +14,7 @@ library.add(faCheckSquare, faCoffee)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
